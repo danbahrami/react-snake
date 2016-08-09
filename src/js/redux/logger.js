b6fd59c959002a31a7cf9bcd5187965dc2876510
danbahrami <dan.bahrami@googlemail.com>
2016-08-09 07:52:24 +0100

    Remove logger middleware

--- src/js/redux/logger.js
+++
@@ -1,6 +0,0 @@
-export default store => next => action => {
-    console.log("dispatching", action)
-    const result = next(action)
-    console.log("next state", store.getState())
-    return result
-}
